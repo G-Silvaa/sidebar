c2a41203de100cee02277c01194a8b6fc1d6461a
G-Silvaa <93489073+G-Silvaa@users.noreply.github.com>
2023-03-24 11:34:46 -0300

    Add files via upload

---
+++ sedebar.js
@@ -0,0 +1,12 @@
+// var menuItem = document.querySelectorAll('.item-menu');
+
+// function selectLink(){
+//     menuItem.forEach((item)=> 
+//         item.classList.remove('ativo')
+//     )
+//     this.classList.add('ativo');
+// }
+ 
+// menuItem((item)=> 
+//     item.addEventListener('click',selectLink)
+// )
